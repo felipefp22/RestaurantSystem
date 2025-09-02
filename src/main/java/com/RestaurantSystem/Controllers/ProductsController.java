167ac21c8f7eeec48308a7ab5f1d75ec46b86062
Felipe Ferreira de Paula <felipefp2@gmail.com>
2025-09-02 02:33:23 -0300

    Products Methods Created

--- src/main/java/com/RestaurantSystem/Controllers/ProductsController.java
+++ src/main/java/com/RestaurantSystem/Controllers/ProductsController.java
@@ -1,64 +1,79 @@
-//package com.RestaurantSystem.Controllers;
-//
-//import com.RestaurantSystem.Entities.Product.DTOs.CreateOrUpdateProductDTO;
-//import com.RestaurantSystem.Entities.Product.Product;
-//import com.RestaurantSystem.Services.ProductService;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Set;
-//import java.util.UUID;
-//
-//@RestController
-//@RequestMapping("/product")
-//public class ProductsController {
-//    private final ProductService productService;
-//
-//    public ProductsController(ProductService productService) {
-//        this.productService = productService;
-//    }
-//
-//    // <>------------ Methods ------------<>
-//    @GetMapping("/get-all-products")
-//    public ResponseEntity<Set<Product>> getAllProducts() {
-//        var response = productService.getAllProducts();
-//
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @GetMapping("/get-products-by-category/{category}")
-//    public ResponseEntity<Set<Product>> getProductsByCategory(@PathVariable String category) {
-//        var response = productService.getProductsByCategory(category);
-//
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @GetMapping("/get-products-by-id/{productId}")
-//    public ResponseEntity<Product> getProductById(@PathVariable UUID productId) {
-//        var response = productService.getProductById(productId);
-//
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @PostMapping("/create-product")
-//    public ResponseEntity<Product> createProduct(@RequestBody CreateOrUpdateProductDTO productToCreate) {
-//        var response = productService.createProduct(productToCreate);
-//
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @PutMapping("/update-product")
-//    public ResponseEntity<Product> updateProduct(@RequestBody CreateOrUpdateProductDTO productToUpdate) {
-//        var response = productService.updateProduct(productToUpdate);
-//
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @DeleteMapping("/delete-product/{productId}")
-//    public ResponseEntity deleteProduct(@PathVariable UUID productId) {
-//
-//         productService.deleteProduct(productId);
-//
-//         return ResponseEntity.noContent().build();
-//    }
-//}
+package com.RestaurantSystem.Controllers;
+
+import com.RestaurantSystem.Entities.Product.DTOs.CreateOrUpdateProductDTO;
+import com.RestaurantSystem.Entities.Product.Product;
+import com.RestaurantSystem.Infra.auth.RetriveAuthInfosService;
+import com.RestaurantSystem.Services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/product")
+public class ProductsController {
+    private final ProductService productService;
+    private final RetriveAuthInfosService retriveAuthInfosService;
+
+    public ProductsController(ProductService productService, RetriveAuthInfosService retriveAuthInfosService) {
+        this.productService = productService;
+        this.retriveAuthInfosService = retriveAuthInfosService;
+    }
+
+    // <>------------ Methods ------------<>
+    @GetMapping("/get-all-products")
+    public ResponseEntity<List<Product>> getAllProducts(@RequestHeader("Authorization") String authorizationHeader) {
+        String requesterID = retriveAuthInfosService.retrieveEmailOfUser(authorizationHeader);
+
+        var response = productService.getAllProducts(requesterID);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get-products-by-category/{category}")
+    public ResponseEntity<List<Product>> getProductsByCategory(@RequestHeader("Authorization") String authorizationHeader,
+                                                              @PathVariable String category) {
+        String requesterID = retriveAuthInfosService.retrieveEmailOfUser(authorizationHeader);
+        var response = productService.getProductsByCategory(requesterID, category);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get-products-by-id/{productId}")
+    public ResponseEntity<Product> getProductById(@RequestHeader("Authorization") String authorizationHeader,
+                                                  @PathVariable UUID productId) {
+        String requesterID = retriveAuthInfosService.retrieveEmailOfUser(authorizationHeader);
+        var response = productService.getProductById(requesterID, productId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/create-product")
+    public ResponseEntity<Product> createProduct(@RequestHeader("Authorization") String authorizationHeader,
+                                                 @RequestBody CreateOrUpdateProductDTO productToCreate) {
+        String requesterID = retriveAuthInfosService.retrieveEmailOfUser(authorizationHeader);
+        var response = productService.createProduct(requesterID, productToCreate);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/update-product")
+    public ResponseEntity<Product> updateProduct(@RequestHeader("Authorization") String authorizationHeader,
+                                                 @RequestBody CreateOrUpdateProductDTO productToUpdate) {
+        String requesterID = retriveAuthInfosService.retrieveEmailOfUser(authorizationHeader);
+        var response = productService.updateProduct(requesterID, productToUpdate);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete-product/{productId}")
+    public ResponseEntity deleteProduct(@RequestHeader("Authorization") String authorizationHeader,
+                                        @PathVariable UUID productId) {
+        String requesterID = retriveAuthInfosService.retrieveEmailOfUser(authorizationHeader);
+        productService.deleteProduct(requesterID, productId);
+
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/RestaurantSystem/Services/ProductService.java
+++ src/main/java/com/RestaurantSystem/Services/ProductService.java
@@ -1,54 +1,126 @@
-//package com.RestaurantSystem.Services;
-//
-//import com.RestaurantSystem.Entities.Product.DTOs.CreateOrUpdateProductDTO;
-//import com.RestaurantSystem.Entities.Product.Product;
-//import com.RestaurantSystem.Repositories.ProductRepo;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//import java.util.UUID;
-//
-//@Service
-//public class ProductService {
-//    private final ProductRepo productRepo;
-//
-//    public ProductService(ProductRepo productRepo) {
-//        this.productRepo = productRepo;
-//    }
-//
-//    // <> ---------- Methods ---------- <>
-//    public Set<Product> getAllProducts() {
-//        Set<Product> response = new HashSet<>(productRepo.findAll());
-//
-//        return response;
-//    }
-//
-//    public Set<Product> getProductsByCategory(String category) {
-//        Set<Product> response = new HashSet<>(productRepo.findAllByCategory(category));
-//
-//        return response;
-//    }
-//
-//    public Product getProductById(UUID productId) {
-//        var response = productRepo.findById(productId).orElseThrow(() -> new RuntimeException("Product not found."));
-//
-//        return response;
-//    }
-//
-//    public Product createProduct(CreateOrUpdateProductDTO productToCreate) {
-//
-//        return productRepo.save(product);
-//    }
-//
-//    public Product updateProduct(CreateOrUpdateProductDTO productToUpdateDTO) {
-//
-//        return productRepo.save(productToUpdate);
-//
-//    }
-//
-//    public void deleteProduct(UUID productId) {
-//        Product productToDelete = productRepo.findById(productId).orElseThrow(() -> new RuntimeException("Product not found."));
-//        productRepo.delete(productToDelete);
-//    }
-//}
+package com.RestaurantSystem.Services;
+
+import com.RestaurantSystem.Entities.Company.Company;
+import com.RestaurantSystem.Entities.Product.DTOs.CreateOrUpdateProductDTO;
+import com.RestaurantSystem.Entities.Product.Product;
+import com.RestaurantSystem.Entities.User.AuthUserLogin;
+import com.RestaurantSystem.Repositories.AuthUserRepository;
+import com.RestaurantSystem.Repositories.CompanyRepo;
+import com.RestaurantSystem.Repositories.ProductRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class ProductService {
+    private final ProductRepo productRepo;
+    private final AuthUserRepository authUserRepository;
+    private final CompanyRepo companyRepo;
+
+    public ProductService(ProductRepo productRepo, AuthUserRepository authUserRepository, CompanyRepo companyRepo) {
+        this.productRepo = productRepo;
+        this.authUserRepository = authUserRepository;
+        this.companyRepo = companyRepo;
+    }
+
+    // <> ---------- Methods ---------- <>
+    public List<Product> getAllProducts(String requesterID) {
+        AuthUserLogin requester = authUserRepository.findById(requesterID)
+                .orElseThrow(() -> new RuntimeException("Requester not found"));
+
+        Company company = companyRepo.findById(UUID.fromString(requester.getCompanyId()))
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+
+        return company.getProducts();
+    }
+
+    public List<Product> getProductsByCategory(String requesterID, String category) {
+        AuthUserLogin requester = authUserRepository.findById(requesterID)
+                .orElseThrow(() -> new RuntimeException("Requester not found"));
+
+        Company company = companyRepo.findById(UUID.fromString(requester.getCompanyId()))
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+
+
+        return company.getProducts().stream().filter(x -> x.getCategory().equals(category)).toList();
+    }
+
+    public Product getProductById(String requesterID, UUID productId) {
+        AuthUserLogin requester = authUserRepository.findById(requesterID)
+                .orElseThrow(() -> new RuntimeException("Requester not found"));
+
+        Company company = companyRepo.findById(UUID.fromString(requester.getCompanyId()))
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+
+        return company.getProducts().stream().filter(x -> x.getId().equals(productId)).findFirst()
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    public Product createProduct(String requesterID, CreateOrUpdateProductDTO productToCreate) {
+        AuthUserLogin requester = authUserRepository.findById(requesterID)
+                .orElseThrow(() -> new RuntimeException("Requester not found"));
+
+        Company company = companyRepo.findById(UUID.fromString(requester.getCompanyId()))
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+
+        if (!company.getManagers().contains(requesterID) && !company.getOwner().equals(requesterID))
+            throw new RuntimeException("You are not allowed to add a product, ask to manager");
+        if (!company.getProductsCategories().contains(productToCreate.category()))
+            throw new RuntimeException("Category not found, create it first");
+
+        Product product = new Product(productToCreate, company, productToCreate.category());
+
+        return productRepo.save(product);
+    }
+
+    public Product updateProduct(String requesterID, CreateOrUpdateProductDTO productToUpdateDTO) {
+        AuthUserLogin requester = authUserRepository.findById(requesterID)
+                .orElseThrow(() -> new RuntimeException("Requester not found"));
+
+        Company company = companyRepo.findById(UUID.fromString(requester.getCompanyId()))
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+
+        if (!company.getManagers().contains(requesterID) && !company.getOwner().equals(requesterID))
+            throw new RuntimeException("You are not allowed to add a product, ask to manager");
+
+        Product productToUpdate = company.getProducts().stream()
+                .filter(x -> x.getId().equals(productToUpdateDTO.id()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        productToUpdate.setName(productToUpdateDTO.name());
+        productToUpdate.setPrice(productToUpdateDTO.price());
+        productToUpdate.setDescription(productToUpdateDTO.description());
+        productToUpdate.setImagePath(productToUpdateDTO.imagePath());
+
+        if (productToUpdateDTO.category() != productToUpdateDTO.category()) {
+            if (!company.getProductsCategories().contains(productToUpdateDTO.category()))
+                throw new RuntimeException("Category not found, create it first");
+
+            productToUpdate.setCategory(productToUpdateDTO.category());
+        }
+
+        return productRepo.save(productToUpdate);
+    }
+
+    public void deleteProduct(String requesterID, UUID productId) {
+        AuthUserLogin requester = authUserRepository.findById(requesterID)
+                .orElseThrow(() -> new RuntimeException("Requester not found"));
+
+        Company company = companyRepo.findById(UUID.fromString(requester.getCompanyId()))
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+
+        if (!company.getManagers().contains(requesterID) && !company.getOwner().equals(requesterID))
+            throw new RuntimeException("You are not allowed to add a product, ask to manager");
+
+        Product productToDelete = company.getProducts().stream()
+                .filter(x -> x.getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        productRepo.delete(productToDelete);
+    }
+}
